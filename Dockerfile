ab3826e9a97ee1c32d21142bbf8b1cfcb79d2b97
Dmitry Kamenskikh <kupnu4x@gmail.com>
2020-09-04 18:22:34 +0800

    email & checks

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM debian:10
 RUN echo 'deb http://deb.debian.org/debian buster-backports main' >> /etc/apt/sources.list.d/buster-backports.list && \
-    apt update && apt install -y default-mysql-client s3cmd ca-certificates && \
+    apt update && apt install -y default-mysql-client s3cmd ca-certificates msmtp && \
     rm -rf /var/lib/apt
 COPY entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,9 +11,30 @@ usage(){
   echo "    S3_SECRET_KEY"
   echo "    S3_HOST (like s3.amazonaws.com)"
   echo "    S3_HOST_BUCKET (like %(bucket)s.s3.amazonaws.com)"
+  echo "    EMAIL_HOST"
+  echo "    EMAIL_FROM"
+  echo "    EMAIL_TO"
 }
-if [[ -z "${MYSQL_HOST}" || -z "${MYSQL_USER}" || -z "${MYSQL_QUERY}" || -z "${S3_FILE_PATH}" || -z "${S3_ACCESS_KEY}" || -z "${S3_SECRET_KEY}" ]]; then
+mail(){
+  subj=$1
+  body=$2
+  echo "To: ${EMAIL_TO}
+From: ${EMAIL_FROM}
+Subject: ${subj}
+
+${body}" | msmtp --EMAIL_FROM="${EMAIL_FROM}" --EMAIL_HOST="${EMAIL_HOST}" "${EMAIL_TO}"
+}
+if [[ -z "${MYSQL_HOST}" || \
+  -z "${MYSQL_USER}" || \
+  -z "${MYSQL_QUERY}" || \
+  -z "${S3_FILE_PATH}" || \
+  -z "${S3_ACCESS_KEY}" || \
+  -z "${S3_SECRET_KEY}" || \
+  -z "${EMAIL_HOST}" || \
+  -z "${EMAIL_FROM}" || \
+  -z "${EMAIL_TO}" ]]; then
   usage
+  mail "etl error" "not all required vars are set"
   exit 1
 fi
 if [[ -z "${S3_HOST}" ]]; then
@@ -23,12 +44,37 @@ if [[ -z "${S3_HOST_BUCKET}" ]]; then
   S3_HOST_BUCKET="%(bucket)s.s3.amazonaws.com"
 fi
 
-#MYSQL_HOST,MYSQL_PWD from env
-mysql -u "${MYSQL_USER}" "${MYSQL_DBNAME}" -B -s -e "${MYSQL_QUERY}" | \
-  sed -e 's/"/""/g' -e "s/\t/\",\"/g;s/^/\"/;s/$/\"/" -e 's/\\t/\t/g;s/\\n/\n/g' | \
-  gzip | \
-  s3cmd put - "${S3_FILE_PATH}" \
-    --access_key="${S3_ACCESS_KEY}" \
-    --secret_key="${S3_SECRET_KEY}" \
-    --host="${S3_HOST}" \
-    --host-bucket="${S3_HOST_BUCKET}"
+LOGFILE=$(mktemp)
+#MYSQL_HOST,MYSQL_PWD EMAIL_FROM env
+mysql -u "${MYSQL_USER}" "${MYSQL_DBNAME}" -e 'quit' >>"${LOGFILE}" 2>&1
+if [ $? -ne 0 ]; then
+  mail "etl error" "cant connect to mysql. some info:
+$(cat "${LOGFILE}")"
+  cat "${LOGFILE}"
+  exit 1
+fi
+DATAFILE=$(mktemp)
+mysql -u "${MYSQL_USER}" "${MYSQL_DBNAME}" -B -s -e "${MYSQL_QUERY}" >"${DATAFILE}" 2>>"${LOGFILE}"
+if [ $? -ne 0 ]; then
+  mail "etl error" "error doing query. some info:
+$(cat "${LOGFILE}")"
+  cat "${LOGFILE}"
+  exit 1
+fi
+
+sed -e 's/"/""/g' -e "s/\t/\",\"/g;s/^/\"/;s/$/\"/" -e 's/\\t/\t/g;s/\\n/\n/g' "${DATAFILE}" | \
+gzip | \
+s3cmd put - "${S3_FILE_PATH}" \
+  --access_key="${S3_ACCESS_KEY}" \
+  --secret_key="${S3_SECRET_KEY}" \
+  --host="${S3_HOST}" \
+  --host-bucket="${S3_HOST_BUCKET}" >>"${LOGFILE}" 2>&1
+if [ $? -ne 0 ]; then
+  mail "etl error" "error doing upload. some info:
+$(cat "${LOGFILE}")"
+  cat "${LOGFILE}"
+  exit 1
+fi
+
+cat "${LOGFILE}"
+rf -rf "${LOGFILE}" "${DATAFILE}"
